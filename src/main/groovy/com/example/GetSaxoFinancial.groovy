1f4b20cd9be9554a6cc85a04809204df7e9fb52e
Kudankita <41056212+Kudankita@users.noreply.github.com>
2020-02-09 17:02:09 +0900

    練習のためにrestclientを使う処理を加えた

--- src/main/groovy/com/example/GetSaxoFinancial.groovy
+++ src/main/groovy/com/example/GetSaxoFinancial.groovy
@@ -1,9 +1,15 @@
 package com.example
 
 import com.example.retailpositioning.RetailPositioningMain
+import groovyx.net.http.HttpContextDecorator
+import groovyx.net.http.RESTClient
 
 class GetSaxoFinancial {
     static main(args) {
-        RetailPositioningMain.retailPositioningMain()
+        // RetailPositioningMain.retailPositioningMain()
+
+        def restClient = new RESTClient('http://dummy.restapiexample.com/')
+        def res = restClient.get(path: "api/v1/employees")
+        println res.status
     }
 }
